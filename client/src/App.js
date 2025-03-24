9d80ed106a6a20c78b0c4d2c62a658f9d7a69267
PradermN <pradermnafack@gmail.com>
2025-03-24 21:25:47 +0100

    pern Todo List

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import React, {Fragment} from 'react';
 import './App.css';
 
+//compoenents
+
+import InputTodo from './components/InputTodo';
+
 function App() {
   return (
-   <Fragment></Fragment>
+   <Fragment>
+    <div className="container">
+      <InputTodo />
+    </div>
+   </Fragment>
   );
 }
 
