e52fb43de746b00f78a8ca9fb91ce1f8b70a7455
PradermN <pradermnafack@gmail.com>
2025-03-24 21:33:10 +0100

    state description

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -1,11 +1,20 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useState} from "react";
 
 const InputTodo = () => {
+
+        // new State, how to set the new state and the default value
+    const [description, setDescription] = useState("")
+
     return (
         <Fragment>
             <h1 className="text-center mt-5">Pern Todo List</h1>
             <form className="d-flex mt-5">
-                <input type="text" className="form-control" />
+                <input
+                    type="text"
+                    className="form-control"
+                    value={description}
+                    onChange={e => setDescription(e.target.value)}
+                    />
                 <button className="btn btn-success">Add</button>
             </form>
         </Fragment>
